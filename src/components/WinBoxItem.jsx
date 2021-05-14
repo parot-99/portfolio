db89d224696b6a7505e1c8752eb1a1ea5a8c7bd0
parot-99 <ahmed-public@hotmail.com>
2021-05-14 23:16:31 +0300

    add responsive behaviour for mobile

--- src/components/WinBoxItem.jsx
+++ src/components/WinBoxItem.jsx
@@ -1,8 +1,31 @@
 import { Fragment, useRef } from 'react'
-import generateWinBox from './../generateWinBox'
+import { useMediaQuery } from 'react-responsive'
+import WinBox from 'winbox/src/js/winbox'
 
 const WinBoxItem = (props) => {
   const ref = useRef()
+  const isMobile = useMediaQuery({ query: '(max-width: 600px)' })
+
+  const generateWinBox = (title, ref) => {
+    new WinBox({
+      title: title,
+      width: isMobile ? '100%' : '600px',
+      height: '400px',
+      x: isMobile ? 'center' : '10%',
+      y: isMobile ? 'center' : '10%',
+      top: isMobile ? '0' : '10%',
+      bottom: isMobile ? '0' : '10%',
+      left: isMobile ? '0' : '10%',
+      right: isMobile ? '0' : '10%',
+      mount: ref.current,
+      onfocus: function () {
+        this.setBackground('#00aa00')
+      },
+      onblur: function () {
+        this.setBackground('#777')
+      },
+    })
+  }
 
   return (
     <Fragment>
